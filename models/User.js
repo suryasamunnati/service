2538bd5c5bfb35c0032554b3e60d04f65a431600
psnmurthy9 <psnmurthy140793@gmail.com>
2025-06-18 14:32:52 +0530

    feat(models): add skippedCompanyInfo field to User schema

--- models/User.js
+++ models/User.js
@@ -29,6 +29,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+  skippedCompanyInfo: {
+    type: Boolean,
+    default: false
+  },
   referralId: {
     type: String,
     unique: true
